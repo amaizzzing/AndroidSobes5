ebd0597a5ed99af9e03f79bf24c2d480ea3e18f7
amaizzzing <amaizzzing@gmail.com>
2021-11-26 00:21:31 +0500

    commit

---
+++ app/src/main/java/com/amaizzzing/sobes5/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.amaizzzing.sobes5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
